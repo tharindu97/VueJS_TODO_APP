8232eca75919ce189d0ffb586b1ac8eb760b0743
tharindu97 <tharindukavishna@gmail.com>
2020-07-09 06:26:52 +0530

    completed_project

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,7 +8,7 @@
                     <form v-on:submit.prevent = "addNewTask">
                         <label for="tasknameinput">Task Name</label>
                         <input v-model="taskname" type="text" id="tasknameinput" class="form-control" placeholder="Add The Task Here">
-                        <label for="tasknameinput">Description</label>
+                        <label class="my-2" for="tasknameinput">Description</label>
                         <input v-model="dscname" type="text" id="dscnameinput" class="form-control" placeholder="Add The Description Here">
                         <button v-if="this.isEdit == false" type="submit" class="btn btn-success btn-block mt-3">
                             Submit
@@ -27,7 +27,7 @@
                          <td class="text-left">{{todo.title}}</td>
                          <td class="text-left">{{todo.description}}</td>
                          <td class="text-right">
-                           <button class="btn btn-info" v-on:click="editTask(todo.title, todo._id)">Edit</button>
+                           <button class="btn btn-info" v-on:click="editTask(todo.title, todo.description, todo._id)">Edit</button>
                            <button class="btn btn-danger" v-on:click="deleteTask(todo._id)">Delete</button>
                          </td>
                         </tr>
@@ -49,6 +49,7 @@ export default {
           todos:[],
           id: '',
           taskname: '',
+          dscname:'',
           isEdit: false
       }
   },
@@ -71,26 +72,29 @@ export default {
 
       },
       addNewTask() {
-          axios.post('/api/save', { title: this.taskname })
+          axios.post('/api/save', { title: this.taskname, description: this.dscname })
           .then((res)=>{
               this.taskname = ''
+              this.dscname = ''
               this.getTasks()
           }).catch((err) => {
               console.log(err)
           })
       },
 
-      editTask(name, id) {
+      editTask(name, desc, id) {
           this.id = id
           this.taskname = name
+          this.dscname = desc
           this.isEdit =true
       },
 
       updateTask() {
           axios.put(`/api/edit/${this.id}`,
-           {title: this.taskname})
+           {title: this.taskname, description: this.dscname})
            .then((res) => {
                this.taskname = ''
+               this.dscname = ''
                this.isEdit = false
                this.getTasks()
                console.log(res)
